931a11f7a12281c9f176cd6de3ae6dfc279a96fd
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-06-29 07:28:18 -0300

    intento jquery

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+     function addItems{
+     	var text = $("#new-text").val();
+     	$("#todolist").append('<p>text</p>');
+     }
+  });
+
+/*$('#textarea1').val('New Text');
+  $('#textarea1').trigger('autoresize');*/
+        
